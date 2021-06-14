95402c662b6b199d1beddce6fbfb0bf808d74a9f
Archit Rungta <architrungta120@gmail.com>
2021-06-14 14:54:50 +0530

    p1 (#6)
    * Changes for BB
    
    * more BB fix
    
    * more fix

--- modules/julia/cmake/init.cmake
+++ modules/julia/cmake/init.cmake
@@ -8,15 +8,8 @@ endif()
 
 # --- Julia ---
 if(WITH_JULIA AND NOT DEFINED Julia_FOUND)
-  include(${CMAKE_CURRENT_LIST_DIR}/FindJulia.cmake)
-  if(NOT Julia_FOUND)
-    message(WARNING "Julia was not found. Disabling Julia bindings...")
-    ocv_module_disable(julia)
-  endif()
-
-  # publish vars for status() dumper
-  set(Julia_FOUND "${Julia_FOUND}" PARENT_SCOPE)
-  set(Julia_EXECUTABLE "${Julia_EXECUTABLE}" PARENT_SCOPE)
+  set(Julia_FOUND "YES" PARENT_SCOPE)
+  set(Julia_EXECUTABLE "dummy_for_bb" PARENT_SCOPE)
   set(HAVE_JULIA "YES" CACHE STRING ADVANCED)
 
 endif()
